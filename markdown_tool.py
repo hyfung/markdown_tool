a08d062e37cc6fee82448f14a66c3c5d22698fa9
hyfung <43932013+hyfung@users.noreply.github.com>
2020-11-15 11:06:25 +0800

    Add files via upload

---
+++ markdown_tool.py
@@ -0,0 +1,55 @@
+#!/usr./bin/python3
+
+import argparse
+import csv
+
+"""
+File format for input:
+col1,col2,col3
+val1,val2,val3
+val4,val5,val6
+
+String format for output:
+|col1|col2|col3|
+|----|----|----|
+|val1|val2|val3|
+|val4|val5|val6|
+
+"""
+
+def leftpad(in_str: str, width: int, pad_char=' ') -> str:
+    """Pad a string from with free character on left side"""
+    if width < len(in_str):
+        return in_str
+    return pad_char * (width - len(in_str)) + in_str
+
+def rightpad(in_str: str, width: int, pad_char=' ') -> str:
+    """Pad a string from with free character on right side"""
+    if width < len(in_str):
+        return in_str
+    return in_str + pad_char * (width - len(in_str))
+
+def get_max_len(arr: 'list of string') -> int:
+    """Get the maximum length of element in a list of string"""
+    return max([len(x) for x in arr])
+
+def find_all_titles(filename: str) -> list:
+    """Returns a list of string starting with '#' """
+    pass
+
+def main():
+    row_width = []
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-f", "--file", required=True, help="Name of CSV file")
+
+    args = vars(ap.parse_args())
+
+    with open(args['file'], newline='') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        for tup in list(zip(*list(csv_reader))):
+            row_width.append(get_max_len(tup))
+    print(row_width)
+
+if __name__ == '__main__':
+    main()
---
+++ test_markdown_tool.py
@@ -0,0 +1,19 @@
+import unittest
+import markdown_tool
+
+class TestMarkdownTool(unittest.TestCase):
+
+    def leftpad_test(self):
+        pass
+
+    def rightpad_test(self):
+        pass
+
+    def get_max_len_test(self):
+        pass
+
+    def find_all_titles(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
